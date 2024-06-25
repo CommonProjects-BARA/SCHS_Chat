afa79862eac3de063f99028219f7079ecdbce601
Alexander Ashevsky <alexander.ashevsky@intravision.ru>
2024-06-25 14:11:47 +0300

    updated user

--- src/backend/SCHS.User.Domain/Entities/User.cs
+++ src/backend/SCHS.User.Domain/Entities/User.cs
@@ -5,6 +5,8 @@ namespace SCHS.User.Domain.Entities
 {
     public class User : Entity<Guid>
     {
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
         public DateTime CreatedOn { get; set; }
         public ActivityStatus ActivityStatus { get; set; }
         public DateTime LastActivityOn { get; set; }
--- src/backend/SCHS.User.Domain/Enums/ActivityStatus.cs
+++ src/backend/SCHS.User.Domain/Enums/ActivityStatus.cs
@@ -1,6 +1,19 @@
-﻿namespace SCHS.User.Domain.Enums
+﻿using System.ComponentModel;
+
+namespace SCHS.User.Domain.Enums
 {
+    /// <summary>
+    /// Статусы активности пользователя
+    /// </summary>
     public enum ActivityStatus
     {
+        [Description("Пользователь активен")]
+        Available,
+        [Description("Не в сети")]
+        Offline,
+        [Description("Отошёл")]
+        AppearAway,
+        [Description("Занят")]
+        Busy
     }
 }

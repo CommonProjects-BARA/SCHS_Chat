d02f5555943ba757fb6b126642a698ca085f6ae8
VladislavTango <zalarderon00@mail.ru>
2024-06-26 19:59:39 +0300

    not final authentication and receiving id from jwt

---
+++ src/backend/AuthenticationService.Api/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using AuthenticationService.Application;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SCHS.User.Domain.Entities;
+using System;
+
+namespace AuthenticationService.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        //[HttpGet("Reg")]
+        //public async Task<IActionResult> Registration(AuthUser user)
+        //{
+        //    AuthService authService;
+        //    return authService.Reg(user);
+        //}
+    }
+}
---
+++ src/backend/AuthenticationService.Infrastructure/DbContext/AuthDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Common.Infrastructure.DataPersistance;
+using Microsoft.EntityFrameworkCore;
+using SCHS.User.Domain.Entities;
+
+namespace AuthenticationService.Infrastructure.DbContext
+{
+    public class AuthDbContext : BaseDbContext
+    {
+        public DbSet<AuthUser> AuthUsers => Set<AuthUser>();
+        public AuthDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}

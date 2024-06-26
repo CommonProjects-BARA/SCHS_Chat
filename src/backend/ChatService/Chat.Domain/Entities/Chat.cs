277a5a1952fad55460f313f94fd95e424a4108e7
Alexander Ashevsky <alexander.ashevsky@intravision.ru>
2024-06-26 16:05:54 +0300

    fix

--- src/backend/ChatService/Chat.Domain/Entities/Chat.cs
+++ src/backend/ChatService/Chat.Domain/Entities/Chat.cs
@@ -5,8 +5,9 @@ namespace Chat.Domain.Entities
 {
     public class Chat : AuditableEntity<int>
     {
-        public ICollection<Message> Messages { get; set; } = new List<Message>();
-        public ICollection<Message> PinnedMessages { get; set; } = new List<Message>();
+        public ICollection<TextMessage> TextMessages { get; set; } = new List<TextMessage>();
+        public ICollection<MediaMessage> MediaMessages { get; set; } = new List<MediaMessage>();
+        public ICollection<int> PinnedMessages { get; set; } = new List<int>();
         public Chat() { }
     }
 }

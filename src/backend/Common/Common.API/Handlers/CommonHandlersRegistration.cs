50dee95599ce27a13af1cf7ea176a8a0cfff75d4
Alexander Ashevsky <alexander.ashevsky@intravision.ru>
2024-06-29 09:58:12 +0300

    made exception handler

---
+++ src/backend/Common/Common.API/Handlers/CommonHandlersRegistration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common.API.Handlers
+{
+    public static class CommonHandlersRegistration
+    {
+        public static IServiceCollection AddMiddlewares(this IServiceCollection services)
+        {
+            services.AddTransient<GlobalExceptionHandler>();
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseMiddlewares(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<GlobalExceptionHandler>();
+
+            return app;
+        }
+    }
+
+}
---
+++ src/backend/Common/Common.API/Handlers/GlobalExceptionHandler.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace Common.API.Handlers
+{
+    public class GlobalExceptionHandler : IMiddleware
+    {
+        private readonly ILogger _logger;
+        public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch(Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                await Handle(context, ex);
+            }
+        }
+        public static async Task Handle(HttpContext context, Exception exception)
+        {
+            var response = context.Response;
+            response.ContentType = "application/json";
+
+            var responseModel = ResponseApi<string>.Fail(exception.Message);
+
+            response.StatusCode = exception switch
+            {
+                ApplicationException e => (int) HttpStatusCode.InternalServerError,
+                ValidationException e => (int) HttpStatusCode.BadRequest,
+                _ => (int)HttpStatusCode.InternalServerError
+            };
+
+            var jsonResponse = JsonSerializer.Serialize(responseModel);
+
+            await response.WriteAsync(jsonResponse);
+        }
+    }
+}

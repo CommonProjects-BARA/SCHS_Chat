c4b96eae623614594be59533c60df5161611a025
Alexander Ashevsky <alexander.ashevsky@intravision.ru>
2024-06-29 13:58:27 +0300

    made cqrs interfaces

--- src/backend/Common/Common.API/Handlers/CommonHandlersRegistration.cs
+++ src/backend/Common/Common.API/Handlers/CommonHandlersRegistration.cs
@@ -5,14 +5,14 @@ namespace Common.API.Handlers
 {
     public static class CommonHandlersRegistration
     {
-        public static IServiceCollection AddMiddlewares(this IServiceCollection services)
+        public static IServiceCollection AddCommonMiddlewares(this IServiceCollection services)
         {
             services.AddTransient<GlobalExceptionHandler>();
 
             return services;
         }
 
-        public static IApplicationBuilder UseMiddlewares(this IApplicationBuilder app)
+        public static IApplicationBuilder UseCommonMiddlewares(this IApplicationBuilder app)
         {
             app.UseMiddleware<GlobalExceptionHandler>();
 
